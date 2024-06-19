e65ab61b47f9b5196b484f9e54c4c837ddc92038
Chandrika <chandrika.mjee@gmail.com>
2024-06-19 01:21:28 -0400

    updated my blogs

--- src/components/BlogsRenderer.js
+++ src/components/BlogsRenderer.js
@@ -1,6 +1,6 @@
-import Collapsible from 'react-collapsible';
+// import Collapsible from 'react-collapsible';
 import React, { useState } from 'react';
-import { ArrowsExpandIcon} from "@heroicons/react/solid";
+// import { ArrowsExpandIcon} from "@heroicons/react/solid";
 import Header from './Header';
 import blogs from '../jsons/blog_list.json';
 import EmptyBlog from './Blogs/EmptyBlog';
@@ -12,7 +12,7 @@ import Personal from './Blogs/Personal';
 
 export default function BlogRenderer(){
     const blog_files = blogs.blogs;
-    const [blog, setBlog] = useState('empty');
+    const [blog, setBlog] = useState('ProgrammingProblems');
     const [showModal, setShowModal] = React.useState(false);
 
     const components = {
@@ -33,7 +33,7 @@ export default function BlogRenderer(){
     return (
         <div>
         <Header/>
-        <div className='mt-2 h-auto bg-lime-200 shadow-lg'>
+        <div className='mt-2 h-auto shadow-lg'>
         <div className="flex flex-col flex-wrap place-content-around pt-2 pb-2">
             {blog_files.map(( cur_blog )=>(
                     <div key={cur_blog} className='m-auto font-mono text-white'>
@@ -53,7 +53,7 @@ export default function BlogRenderer(){
                   <p className="text-fuchsia-900 text-lg leading-relaxed">
                     Selected Blog - {component_val[blog]}
                   </p>
-                  <p className='text-slate-500 text-sm leading-relaxed'>click the expand arrow to read/close</p>
+                  {/* <p className='text-slate-500 text-sm leading-relaxed'>click the expand arrow to read/close</p> */}
                 </div>
                 {/*footer*/}
                 <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
@@ -77,7 +77,7 @@ export default function BlogRenderer(){
         </div>
     </div>
     <div className='mt-2 bg-neutral-200 shadow-lg'>
-        <Collapsible trigger={<ArrowsExpandIcon className="w-10 h-10 mx-auto text-center hover:bg-indigo-700 rounded " />}>
+        {/* <Collapsible trigger={<p className="w-fit h-auto mx-auto text-center py-1 px-3 hover:bg-blue-200 rounded " >Expand &#8594;</p>}> */}
         
         {(() => {
             const SpecificBlog = components[blog];
@@ -85,7 +85,7 @@ export default function BlogRenderer(){
              
         })()}
                                 
-        </Collapsible>
+        {/* </Collapsible> */}
 
     </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 export default function Header(){
     return (
         <div>
-            <div className='bg-cyan-500 h-14 flex justify-center shadow-lg'>
-            <a href="https://chandms.github.io/" className='m-auto font-mono text-white border-0 py-1 px-3 hover:bg-indigo-900 rounded'>Chandrika Mukherjee</a>
+            <div className='bg-slate-100 h-14 flex justify-center shadow-lg'>
+            <a href="https://chandms.github.io/" className='m-auto text-blue-800 font-mono border-0 py-1 px-3 hover:bg-blue-200 rounded'>Chandrika Mukherjee</a>
         </div>
-        <div className='bg-teal-900 h-14 flex justify-center shadow-lg'>
-            <h1 className='m-auto font-mono text-white'>Blogs</h1>
+        <div className='bg-slate-400 h-14 flex justify-center shadow-lg'>
+            <h1 className='m-auto font-mono text-white'>Welcome to My Blogs</h1>
         </div>
         </div>
     );
